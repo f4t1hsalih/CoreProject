1f8f61212b93631401b0bf09b3a5a838440a5d70
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-13 13:35:28 +0300

    Lecture 15 - Generic Repository

---
+++ DataAccessLayer/Repository/AboutRepository.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Repository
+{
+    public class AboutRepository : IAboutDal
+    {
+        public void Add(About entity)
+        {
+            using var c = new Context();
+            c.Abouts.Add(entity);
+            c.SaveChanges();
+        }
+
+        public void Delete(About entity)
+        {
+            using var c = new Context();
+            c.Abouts.Remove(entity);
+            c.SaveChanges();
+        }
+
+        public About GetById(int id)
+        {
+            using var c = new Context();
+            return c.Abouts.Find(id);
+        }
+
+        public List<About> GetListAll()
+        {
+            using var c = new Context();
+            return c.Abouts.ToList();
+        }
+
+        public void Update(About entity)
+        {
+            using var c = new Context();
+            c.Abouts.Update(entity);
+        }
+    }
+}
---
+++ DataAccessLayer/Repository/GenericRepositoryDal.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepositoryDal<T> : IGenericDal<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            using var c = new Context();
+            c.Add(entity);
+            c.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            using var c = new Context();
+            c.Remove(entity);
+            c.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public void Update(T entity)
+        {
+            using var c = new Context();
+            c.Update(entity);
+            c.SaveChanges();
+        }
+    }
+}
