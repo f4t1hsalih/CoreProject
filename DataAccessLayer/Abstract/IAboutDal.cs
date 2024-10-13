f1b0b70a9e8c232c868153223b7b73c721c24fdb
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-13 12:53:41 +0300

    Lecture 13 - Interfacelerin Yazılması

---
+++ DataAccessLayer/Abstract/IAboutDal.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IAboutDal
+    {
+        void Insert(About about);
+        void Delete(About about);
+        void Update(About about);
+        List<About> GetListAll();
+
+    }
+}
---
+++ DataAccessLayer/Abstract/ISkillDal.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ISkillDal
+    {
+        void Add(Skill skill);
+        void Update(Skill skill);
+        void Delete(Skill skill);
+        List<Skill> GetListAll();
+
+    }
+}
