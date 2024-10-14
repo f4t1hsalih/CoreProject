3176e6d0f73916b7324a74c596cf32a0d27941a3
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-14 15:33:14 +0300

    Lecture 18 - Generic Service

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T>
+    {
+        List<T> TGetListAll();
+        T TGetById(int id);
+        void TAdd(T entity);
+        void TUpdate(T entity);
+        void TDelete(T entity);
+    }
+}
