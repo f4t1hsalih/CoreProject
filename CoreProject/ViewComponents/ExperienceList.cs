c7ba7b21bb4ea0b552aa557e32834622e1060865
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-15 21:11:37 +0300

    Lecture 34-35  - Experience List

---
+++ CoreProject/ViewComponents/ExperienceList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents
+{
+    public class ExperienceList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+            var values = experienceManager.TGetListAll();
+            return View(values);
+        }
+    }
+}
