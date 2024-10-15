e50cac75743eeb2bb3c9de08608c6aff3f479a83
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-15 19:23:35 +0300

    Lecture 27 - About List

---
+++ CoreProject/ViewComponents/AboutList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents
+{
+    public class AboutList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            AboutManager aboutManager = new AboutManager(new EfAboutDal());
+            var values = aboutManager.TGetListAll();
+            return View(values);
+        }
+    }
+}
