d12544a270a0a0c161ee419cdc3cc262a6831251
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-16 14:09:54 +0300

    Lecture 36 - Testimonial

---
+++ CoreProject/ViewComponents/TestimonialList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents
+{
+    public class TestimonialList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+            var values = testimonialManager.TGetListAll();
+            return View(values);
+        }
+    }
+}
