790ce65f881644628fcbd5f63f6f23da592227d4
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-17 09:48:10 +0300

    Lecture 45 - Yeteneklerin Listelenmesi

---
+++ CoreProject/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class SkillController : Controller
+    {
+        public IActionResult Index()
+        {
+            SkillManager skillManager = new SkillManager(new EfSkillDal());
+            var skillValues = skillManager.TGetListAll();
+            return View(skillValues);
+        }
+    }
+}
