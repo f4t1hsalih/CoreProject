7d59d54aa3d09a01337ad3e840184f610a61d5ff
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-17 11:02:23 +0300

    Lecture 47 - Yeni Yetenek Ekleme İşlemi

--- CoreProject/Controllers/SkillController.cs
+++ CoreProject/Controllers/SkillController.cs
@@ -1,16 +1,36 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreProject.Controllers
 {
     public class SkillController : Controller
     {
+
         public IActionResult Index()
         {
+            ViewBag.PageName = "Yetenek Listesi";
+            ViewBag.NewButton = "/Skill/AddSkill/";
+
             SkillManager skillManager = new SkillManager(new EfSkillDal());
             var skillValues = skillManager.TGetListAll();
             return View(skillValues);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.PageName = "Yetenek Ekle";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            ViewBag.PageName = "Yetenek Ekle";
+            SkillManager skillManager = new SkillManager(new EfSkillDal());
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
         }
     }
 }
