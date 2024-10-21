f03edd9a5525c04f884052ec3e7a8f52ec652878
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-21 21:09:35 +0300

    Testimonial İşlemleri

---
+++ BusinessLayer/ValidationRules/TestimonialValidator.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class TestimonialValidator : AbstractValidator<Testimonial>
+    {
+        public TestimonialValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("İsim alanı boş bırakılamaz.")
+                .MaximumLength(100).WithMessage("İsim alanı en fazla 100 karakter olmalıdır.");
+
+            RuleFor(x => x.Position)
+                .NotEmpty().WithMessage("Pozisyon alanı boş bırakılamaz.")
+                .MaximumLength(50).WithMessage("Pozisyon alanı en fazla 50 karakter olmalıdır.");
+
+            RuleFor(x => x.Company)
+                .NotEmpty().WithMessage("Şirket alanı boş bırakılamaz.")
+                .MaximumLength(100).WithMessage("Şirket alanı en fazla 100 karakter olmalıdır.");
+
+            RuleFor(x => x.Comment)
+                .NotEmpty().WithMessage("Yorum alanı boş bırakılamaz.")
+                .MaximumLength(500).WithMessage("Yorum alanı en fazla 500 karakter olmalıdır.");
+
+            RuleFor(x => x.ImageUrl)
+                .NotEmpty().WithMessage("Resim alanı boş bırakılamaz.")
+                .MaximumLength(200).WithMessage("Resim URL'si alanı en fazla 200 karakter olmalıdır.");
+        }
+    }
+}
---
+++ CoreProject/Controllers/TestimonialController.cs
@@ -0,0 +1,79 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        void ViewBags(string pageName)
+        {
+            ViewBag.PageName = pageName;
+            ViewBag.NewButton = "/Testimonial/AddTestimonial/";
+        }
+        public IActionResult Index()
+        {
+            ViewBags("Referans Listesi");
+            var values = testimonialManager.TGetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            ViewBags("Referans Ekle");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial testimonial)
+        {
+            TestimonialValidator rules = new TestimonialValidator();
+            ValidationResult results = rules.Validate(testimonial);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+            testimonialManager.TAdd(testimonial);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditTestimonial(int id)
+        {
+            ViewBags("Referans Düzenle");
+            var value = testimonialManager.TGetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult EditTestimonial(Testimonial testimonial)
+        {
+            TestimonialValidator rules = new TestimonialValidator();
+            ValidationResult results = rules.Validate(testimonial);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+            testimonialManager.TUpdate(testimonial);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteTestimonial(int id)
+        {
+            testimonialManager.TDelete(testimonialManager.TGetById(id));
+            return RedirectToAction("Index");
+        }
+
+    }
+}
