56a9a239bfcdc8e7e47331874acc16d1195e1d89
f4t1hsalih <f4t1hsalih@gmail.com>
2024-10-31 19:00:33 +0300

    Lecture 182-183 - SocialMedia Kısmını ViewComponenet Olarak Ayarlama

---
+++ CoreProject/ViewComponents/SocialMedia/SocialMediaList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.SocialMedia
+{
+    public class SocialMediaList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+            var values = socialMediaManager.TGetListAll();
+            return View(values);
+        }
+    }
+}
